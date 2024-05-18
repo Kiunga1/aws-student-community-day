aba1823953ff0fdef708548b1fd631d667c39b9c
ann5306 <annkiunga@gmail.com>
2024-05-18 12:15:42 +0300

    Add Countdown.jsx component and Countdown.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@
 
 import About from "./components/About"
 import Agenda from "./components/Agenda"
+import Countdown from "./components/Countdown"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 
 const App = () => {
+  // Set the event date to September 28, 2024, at 6:00 AM UTC which is equivalent to 28th September 2024 East African Time 9 am
+  const eventDate = new Date('2024-09-28T06:00:00Z');
+
   return (
     <div className="bg-gray-light">
       <Header/>
       <Hero/>
       <About/>
       <Agenda/>
+      <Countdown eventDate={eventDate}/>
     </div>
   )
 }
