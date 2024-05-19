9174d7a7d43a8398b30f7eae9fe624489e7856dd
ann5306 <annkiunga@gmail.com>
2024-05-19 09:55:10 +0300

    Add VenueSection.js component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Agenda from "./components/Agenda"
 import Countdown from "./components/Countdown"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import VenueSection from "./components/VenueSection"
 
 const App = () => {
-  // Set the event date to September 28, 2024, at 6:00 AM UTC which is equivalent to 28th September 2024 East African Time 9 am
+  // Set the event date to September 28, 2024, at 6:00 AM UTC 
+  // which is equivalent to 28th September 2024 East African Time 9 am
   const eventDate = new Date('2024-09-28T06:00:00Z');
 
   return (
@@ -17,6 +19,7 @@ const App = () => {
       <About/>
       <Agenda/>
       <Countdown eventDate={eventDate}/>
+      <VenueSection/>
     </div>
   )
 }
--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -4,10 +4,16 @@ const Agenda = () => {
   
   const agendaData = [
     {
-      time: '9:00 am - 9:30 am',
-      sessionTitle: 'Welcome & opening remarks',
+      time: '9:00 am - 10:30 am',
+      sessionTitle: 'Keynotes',
       speakers: ['AWS Student Community Kenya Team'],
-      description: '',
+      description: '-',
+    },
+    {
+      time: '10:30 am - 11:00 am',
+      sessionTitle: 'Cloud Computing Fundamentals(Beginner Track)',
+      speakers: ['John Doe, AWS Cloud Architect'],
+      description: 'This session provides a fundamental understanding of cloud computing concepts... ',
     },
     {
       time: '9:30 am - 10:30 am',
@@ -16,7 +22,19 @@ const Agenda = () => {
       description: 'This session provides a fundamental understanding of cloud computing concepts... ',
     },
     {
-      time: '9:30 am - 10:30 am',
+      time: '10:30 am - 11:00 am',
+      sessionTitle: 'Cloud Computing Fundamentals(Beginner Track)',
+      speakers: ['John Doe, AWS Cloud Architect'],
+      description: 'This session provides a fundamental understanding of cloud computing concepts... ',
+    },
+    {
+      time: '10:30 am - 11:00 am',
+      sessionTitle: 'Cloud Computing Fundamentals(Beginner Track)',
+      speakers: ['John Doe, AWS Cloud Architect'],
+      description: 'This session provides a fundamental understanding of cloud computing concepts... ',
+    },
+    {
+      time: '10:30 am - 11:00 am',
       sessionTitle: 'Cloud Computing Fundamentals(Beginner Track)',
       speakers: ['John Doe, AWS Cloud Architect'],
       description: 'This session provides a fundamental understanding of cloud computing concepts... ',
@@ -25,12 +43,12 @@ const Agenda = () => {
   ];
 
   return (
-    <div className='p-10'>
+    <section id='agenda' className='p-10 font-sans'>
       <h2 className='text-center text-2xl font-bold mb-8'>Event Agenda</h2>
       <table className="w-full border-collapse border-none rounded">
         <thead className="bg-black text-white p-4">
           <tr>
-            <th className="px-4 py-3">Time</th>
+            <th className="px-20 py-3">Time</th>
             <th className="px-4 py-3">Session Title</th>
             <th className="px-4 py-3">Speaker(s)</th>
             <th className="px-4 py-3">Description</th>
@@ -39,7 +57,7 @@ const Agenda = () => {
         <tbody>
           {agendaData.map((item, index) => (
             <tr key={index} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-dark'}>
-              <td className="border px-4 py-2">{item.time}</td>
+              <td className="border px-4 py-2 font-semibold">{item.time}</td>
               <td className="border px-4 py-2">{item.sessionTitle}</td>
               <td className="border px-4 py-2">{item.speakers.join(', ')}</td>
               <td className="border px-4 py-2">{item.description}</td>
@@ -47,7 +65,7 @@ const Agenda = () => {
           ))}
         </tbody>
       </table>
-    </div>
+    </section>
   );
 };
 
